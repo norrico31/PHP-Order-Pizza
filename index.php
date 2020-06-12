090e6f6dc39a91339fbb9b7129d91213815cec9a
norrico31 <geraldbiasonjob31.com>
2020-06-12 21:06:58 +0800

    templates for header and footer using materialize css

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
 </head>
-<body>
+    <?php include 'templates/header.php'; ?>
     
-</body>
+    <?php include 'templates/footer.php'; ?>
 </html>
