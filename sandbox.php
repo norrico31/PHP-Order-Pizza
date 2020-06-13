96c497ce1d89a44e61970e0f94a48a5b5024574e
norrico31 <geraldbiasonjob31.com>
2020-06-13 23:24:20 +0800

    ternary operator in php and dom

---
+++ sandbox.php
@@ -0,0 +1,18 @@
+<?php 
+    $score = 50;
+
+    // $result = $score > 30 ? 'high score' : 'low score';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ternary Operator</title>
+</head>
+<body>
+    <!-- <p><?php echo $result; ?></p> -->
+
+    <p><?php echo $score > 40 ? 'high score!' : 'low score'; ?></p>
+</body>
+</html>
