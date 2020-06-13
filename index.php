e59c5bfb68581194bef92c675c0426fc152c767a
norrico31 <geraldbiasonjob31.com>
2020-06-13 18:48:23 +0800

    mysqli takes 4 arguments which is the localhost, the username, password and database name(not name of database table)

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
+    // connect to databse
+    $conn = mysqli_connect('localhost', 'order_pizza', 'pizza1234', 'order_pizza');
 
+    // check connection
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
