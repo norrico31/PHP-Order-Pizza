3708b3bcda6036d50d92a0a02f6729bd4afd38dc
norrico31 <geraldbiasonjob31.com>
2020-06-14 00:12:10 +0800

    superglobal used in add.php in form action

--- sandbox.php
+++ sandbox.php
@@ -2,6 +2,12 @@
     $score = 50;
 
     // $result = $score > 30 ? 'high score' : 'low score';
+
+    // superglobals
+    echo $_SERVER['SERVER_NAME']. '<br />';
+    echo $_SERVER['REQUEST_METHOD'] . '<br />';
+    echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+    echo $_SERVER['PHP_SELF'] . '<br />';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +19,6 @@
 <body>
     <!-- <p><?php echo $result; ?></p> -->
 
-    <p><?php echo $score > 40 ? 'high score!' : 'low score'; ?></p>
+    <!-- <p><?php echo $score > 40 ? 'high score!' : 'low score'; ?></p> -->
 </body>
 </html>
