3c368b3b082755c7456b4d86e42d932076f21210
norrico31 <geraldbiasonjob31.com>
2020-06-13 19:11:17 +0800

    sql queries to get the records from pizzas database and make it to associative array

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     if (!$conn) {
         echo 'Connection error: ' . mysqli_connect_error();
     }
+
+    // write query for all record in pizzas
+    $sql =  'SELECT title, ingredients, id FROM pizzas';
+
+    // make query and get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+    // free result from memory
+    mysqli_free_result($result);
+
+    // close the db connection
+    mysqli_close($conn);
+    
+    print_r($pizzas);
 ?>
 <!DOCTYPE html>
 <html lang="en">
