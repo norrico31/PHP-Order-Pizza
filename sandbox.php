44e1902ef956d2723453a27ec5c94bfa196075c5
norrico31 <geraldbiasonjob31.com>
2020-06-14 14:52:22 +0800

    set the gender cookie in sandbox and header.php

--- sandbox.php
+++ sandbox.php
@@ -1,11 +1,11 @@
 <?php 
    // sessions
     if (isset($_POST['submit'])) {
+        // cookie for gender
+        setcookie('gender', $_POST['gender'], time() + 86400);
+
         session_start();
-
         $_SESSION['name'] = $_POST['name'];
-
-        // echo $_SESSION['name'];
         header('Location: index.php');
     }
 ?>
@@ -19,6 +19,10 @@
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
         <input type="text" name="name">
+        <select name="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select>
         <input type="submit" name="submit" value="Submit">
     </form>
 </body>
